c2b258774819779615b74b0cd1f8a861f1918e73
grizz03 <sawilson1110@yahoo.com>
2021-04-27 20:57:16 -0700

    added random ducks

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -16,14 +17,26 @@ water_bg = pygame.image.load('./shooting range assets/Water_BG.png')
 cloud1 = pygame.image.load('./shooting range assets/Cloud1.png')
 cloud2 = pygame.image.load('./shooting range assets/Cloud2.png')
 crosshair = pygame.image.load('./shooting range assets/crosshair.png')
+duck = pygame.image.load('./shooting range assets/duck.png')
 
 # Animations
-land_position_y = 550
+land_position_y = 600
 land_speed = 1
-
 water_position_y = 620
 water_speed = 1
 
+# Creating duck list to place ducks everywhere
+duck_list = []
+
+# Repeats 20 objects
+for x in range(20):
+    # Generating random number for rectangle position
+    duck_position_x = random.randrange(50,1200)
+    duck_position_y = random.randrange(120,630)
+    duck_rect = duck.get_rect(center=(duck_position_x,duck_position_y))
+    duck_list.append(duck_rect)
+
+# MAIN GAME LOOP
 while True:
     # While loop for closing game
     for event in pygame.event.get():
@@ -37,6 +50,15 @@ while True:
     # Applying background (background always on top)
     screen.blit(wood_bg, (0, 0))
 
+    screen.blit(cloud1, (150, 50))
+    screen.blit(cloud1, (600, 150))
+    screen.blit(cloud2, (800, 100))
+    screen.blit(cloud2, (300, 100))
+
+    # duck image
+    for duck_rect in duck_list:
+        screen.blit(duck, duck_rect)
+
     # Animation ranges of water
     # Makes water move down the y-axis
     water_position_y -= water_speed
@@ -49,10 +71,6 @@ while True:
     # Setting images in game
     screen.blit(land_bg, (0, land_position_y))
     screen.blit(water_bg, (0, water_position_y))
-    screen.blit(cloud1, (150, 50))
-    screen.blit(cloud1, (600, 150))
-    screen.blit(cloud2, (800, 100))
-    screen.blit(cloud2, (300, 100))
     # constant game loop with update()
     pygame.display.update()
     # framerate of game
