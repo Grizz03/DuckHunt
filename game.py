7f5a9102a932a19d3b1ff6c6abf4b8c1fa596eda
grizz03 <sawilson1110@yahoo.com>
2021-04-27 19:39:11 -0700

    added background to game

--- game.py
+++ game.py
@@ -1,2 +1,28 @@
 import pygame
+import sys
 
+pygame.init()
+
+# initialize screen size
+screen = pygame.display.set_mode((1280, 720))
+
+# creates clock object for same framerate for every computer
+clock = pygame.time.Clock()
+
+# importing images
+wood_bg = pygame.image.load('./shooting range assets/Wood_BG.png')
+
+
+while True:
+    # while loop for closing game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+
+    # applying background
+    screen.blit(wood_bg,(0,0))
+    pygame.display.update()
+    clock.tick(60)
